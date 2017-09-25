c6761d17249c8edbb22cdb87386bad04f7e0953d
yearn <yearn@ablemobile.com>
2017-09-25 17:46:38 +0800

    update

--- Angular2/gulpfile.js
+++ Angular2/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require("gulp"),
     extreplace = require('gulp-ext-replace'),
     typescript = require('gulp-typescript'),
+    less = require("gulp-less"),
     rename = require("gulp-rename"),
     minifyCss = require("gulp-minify-css"),    //壓縮css
     minifyHtml = require("gulp-minify-html"),  //壓縮html
@@ -12,50 +13,69 @@ var gulp = require("gulp"),
 
 var tsProject = typescript.createProject('tsconfig.json');
 
+//less
+gulp.task("less", function () {
+    return gulp.src("src/less/**/*.less")  //原文件
+    .pipe(less())
+    .pipe(rename({ suffix: '.min' }))  
+    .pipe(minifyCss())
+    .pipe(gulp.dest("assets/css"));        //目標目錄
+});
 //壓縮css
 gulp.task("minify-css", function () {
-    return gulp.src("assets/css/**/*.css")  //原文件
+    return gulp.src("src/css/**/*.css")  //原文件
     .pipe(rename({ suffix: '.min' }))  //在原文件名後面加.min
     .pipe(minifyCss())
-    .pipe(gulp.dest("dist/css"));        //目標目錄
-});
-//合併js
-gulp.task("concat", function () {
-    return gulp.src("assets/js/**/*.js")
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js"))
+    .pipe(gulp.dest("assets/css"));        //目標目錄
 });
 //壓縮js
 gulp.task("uglify", function () {
-    return gulp.src("assets/js/**/*.js")
+    return gulp.src("src/js/**/*.js")
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest("dist/js"))
+    .pipe(gulp.dest("assets/js"))
 });
 gulp.task('buildts', function () {
-    return gulp.src("assets/ts/**/*.ts")
+    return gulp.src("src/ts/**/*.ts")
         .pipe(rename({ suffix: '.min' }))
         .pipe(typescript(tsProject))
         .pipe(uglify())
         .pipe(extreplace('.js'))
-        .pipe(gulp.dest("dist/js"));
+        .pipe(gulp.dest("assets/js"));
 });
 gulp.task('buildtsapp', function () {
-    return gulp.src("assets/app/**/*.ts")
-        .pipe(typescript(tsProject))
+    return gulp.src("src/app/**/*.ts")
+        //.pipe(typescript(appTsProject))
         .pipe(uglify())
         .pipe(extreplace('.js'))
-        .pipe(gulp.dest("app"));
+        .pipe(gulp.dest("assets/app"));
 });
 
+//合併css
+//gulp.task("bootstrapcss", function () {
+//    return gulp.src("src/cless/bootstrap/**/*.less")
+//    .pipe(less())
+//    .pipe(concat('bootstrap.css'))
+//    .pipe(minifyCss())
+//    .pipe(gulp.dest("assets/css"))
+//});
+
+//合併js
+//gulp.task("concatjs", function () {
+//    return gulp.src("src/js/**/*.js")
+//    .pipe(concat('all.js'))
+//    .pipe(uglify())
+//    .pipe(gulp.dest("assets/js"))
+//});
+
 //默認任務，後面為默認執行的所有任務
-gulp.task("default", ["minify-css", "concat", "uglify", "buildts", "watchl"]);   //執行defaul
+gulp.task("default", ["less", "minify-css", "uglify", "buildts", "buildtsapp", "watchl"]);   //執行defaul
 
 //監聽文件
 gulp.task("watchl", function () {
-    gulp.watch("assets/css/**/*.css", ['minify-css']);
-    gulp.watch("assets/js/**/*.js", ['concat', "uglify"]);
-    gulp.watch("assets/ts/**/*.ts", ["buildts"]);
-    //gulp.watch("assets/app/**/*.ts", ["buildtsapp"]);
+    gulp.watch("src/less/**/*.less", ["less"]);
+    gulp.watch("src/css/**/*.css", ["inify-css"]);
+    gulp.watch("src/js/**/*.js", ["uglify"]);
+    gulp.watch("src/ts/**/*.ts", ["buildts"]);
+    gulp.watch("src/app/**/*.ts", ["buildtsapp"]);
 });
